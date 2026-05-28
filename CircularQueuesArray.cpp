7a93dca41bb0153240c77d54ba66bd8205f595de
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-28 11:21:36 +0700

    menu utama dengan blok try-catch

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -89,4 +89,22 @@ int main()
 {
     Queues q;
     char ch;
+    while (true)
+    {
+        try
+        {
+        cout <<"Menu" << endl;
+        cout << "1. Implement insertt operation" << endl;
+        cout << "2. Implement Delete Operation" << endl;
+        cout << "3. Display values" << endl;
+        cout << "4. Exit " << endl;
+        cin >> ch;
+        cout << endl;
+        }
+        catch (exception& e)
+        {
+            cout << "check for the vlaues entered" << endl;
+        }
+        
+    }
 }
