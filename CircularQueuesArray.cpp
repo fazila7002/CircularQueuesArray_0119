8d8abe32f4de2d1e3c3e8168a51c894e75ad8327
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-28 10:55:35 +0700

    if untuk pastiin indeks antrian tidak melingkar, trus while untuk mencetak datanya satu per satu dari FRONT ke REAR

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -55,6 +55,18 @@ class Queues
             return;
         }
         cout << "Element in the queue are......\n";
+
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+            
+        }
+        
         
 
     
