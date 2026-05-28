2ca7c302aff00f5eb8df381ba6b92a6bdcf9ffb5
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-28 10:27:16 +0700

    fungsi insert untuk menerima input data

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -15,4 +15,12 @@ class Queues
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+    }
 };
