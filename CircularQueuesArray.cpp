e93d970a52c2e65349f33c36858938a516689bd8
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-28 11:29:18 +0700

    tambahkan switch untuk pilihan menunya

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -44,6 +44,7 @@ class Queues
         
     }
 
+
     void display()
     {
         int FRONT_Position = FRONT;
@@ -100,11 +101,40 @@ int main()
         cout << "4. Exit " << endl;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            q.insert();
+            break;
+        }
+        case '2':
+        {
+            q.remove();
+            break;
+        }
+        case '3':
+        {
+            q.display();
+            break;
+
+        }
+        case '4':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid opstion !!" << endl;
+            break;
+        }
+        }
         }
         catch (exception& e)
         {
             cout << "check for the vlaues entered" << endl;
         }
-        
+        return 0;
     }
 }
